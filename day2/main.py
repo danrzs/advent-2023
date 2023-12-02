2183a824e5bbce9cfb123cc12099d783604432cf
Daniel Smith <daniel1dean@gmail.com>
2023-12-02 14:13:29 +0000

    part 2 solution

--- day2/main.py
+++ day2/main.py
@@ -4,10 +4,17 @@ from enum import Enum
 import re 
 from typing import List, Dict, Union
 import itertools
+import math
 
 Pull = Dict[str, int]
 Round = List[Pull]
 Game = Dict[str, Union[List[Round], int]]
+
+pull_maxes = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
 
 def _parse_round(round: str) -> Round:
     rounddata = {}
@@ -24,12 +31,6 @@ def parse_input(line: str) -> Game:
     }
 
 def part1(games: List[Game]) -> None:
-    pull_maxes = {
-        "red": 12,
-        "green": 13,
-        "blue": 14,
-    }
-
     def _is_game_bad(game) -> bool:
         for round in game["rounds"]:
             if any([round.get(key, 0) > pull_maxes[key] for key in pull_maxes.keys()]):
@@ -39,7 +40,24 @@ def part1(games: List[Game]) -> None:
     possible_games = [pg["game_id"] for pg in itertools.filterfalse(_is_game_bad, games)]
 
     print("Games {} are possible".format(', '.join(map(str, possible_games))))
-    print("Their sum is {}".format(sum(possible_games)))
+    print(f"Their sum is {sum(possible_games)}")
+
+
+def part2(games: List[Game]) -> None:
+    def _powerup_game(game: Game) -> int:
+        pull_mins = {
+            "red": 0,
+            "green": 0,
+            "blue": 0,
+        }
+        for round in game["rounds"]:
+            pull_mins.update({key: round[key] for key in pull_mins.keys() if round.get(key, 0) > pull_mins[key]})
+
+        return math.prod(pull_mins.values())
+
+    powers = list(map(_powerup_game, games))
+    [print(f"Game {i} has a power of {p}") for i, p in enumerate(powers, start=1)]
+    print(f"The sum of these powers is {sum(powers)}")
 
 
 print("Part 1 known values: ")
@@ -48,3 +66,8 @@ part1([parse_input(line) for line in data.part1known.splitlines() if line])
 print("Part 1 data: ")
 part1([parse_input(line) for line in data.part1data.splitlines() if line])
 
+print("Part 2 known values: ")
+part2([parse_input(line) for line in data.part1known.splitlines() if line])
+
+print("Part 2 data: ")
+part2([parse_input(line) for line in data.part1data.splitlines() if line])
